23f1e1bf07e7bb13feae4b1630822dff149ac9df
Thomas Culp <culp@college.harvard.edu>
2016-03-07 17:37:02 -0500

    I like where this is conceptually right now.

--- assignment3/newspaper/content/models.py
+++ assignment3/newspaper/content/models.py
@@ -7,15 +7,46 @@ class Content(models.Model):
   subtitle = models.CharField(max_length = 500)
   contributors = models.ManyToManyField('Contributor', related_name = 'content')
   pub_date = models.DateTimeField('date published')
+'''
+  def format_contribs():
+    contribs = ""
+    if len(contributors) == 1:
+      contribs += 'Contributor: '
+    else:
+      contribs += 'Contributors: '
+    for c in contributors:
+      contribs += (c + ', ')
+    return contribs[:-2]
+'''
 
 class Article(Content):
-  pass
+#use TextField for articles
+  
+  text = models.TextField(default = 'put yo article right up in here')
+  '''
+  def show(): 
+    print '\n'
+    print 'The Harvard Crimson\n'
+    print pub_date.strftime('%A %d %B %Y\n')
+    print title
+    print '{0}\n'.format(super(Article, self).format_contribs())
+    print textwrap.fill(text, 80)
+    print '\n'
+  '''
 
 class Image(Content):
-  pass
+  path = models.CharField(default = 'path', max_length = 500)
 
 class Contributor(models.Model):
-  pass
+  first_name = models.CharField(default='first name', max_length = 50) 
+  last_name = models.CharField(default='last name', max_length = 50) 
+  content = Content.objects.filter(contributors = objects.filter(first_name =
+    first_name).filter(last_name = last_name))
+
+  def die():
+    objects.filter(first_name = first_name).filter(last_name =
+        last_name).delete()
+
 
 # I need to implement all of these models, but that is currently for another
 # day. That seems like a lot of work at the moment. Check back in assignment3.md
