6349308376e5ea6531f178552869f7da73396012
Thomas Culp <culp@college.harvard.edu>
2016-03-09 16:54:50 -0500

    It fuckin works

--- assignment3/newspaper/content/models.py
+++ assignment3/newspaper/content/models.py
@@ -20,32 +20,22 @@ class Content(models.Model):
 '''
 
 class Article(Content):
-#use TextField for articles
-  
   text = models.TextField(default = 'put yo article right up in here')
-  '''
-  def show(): 
-    print '\n'
-    print 'The Harvard Crimson\n'
-    print pub_date.strftime('%A %d %B %Y\n')
-    print title
-    print '{0}\n'.format(super(Article, self).format_contribs())
-    print textwrap.fill(text, 80)
-    print '\n'
-  '''
+  
+  def show(self):
+    print self.title + ": " + self.text
+
 
 class Image(Content):
   path = models.CharField(default = 'path', max_length = 500)
 
 class Contributor(models.Model):
-  first_name = models.CharField(default='first name', max_length = 50) 
-  last_name = models.CharField(default='last name', max_length = 50) 
-  content = Content.objects.filter(contributors = objects.filter(first_name =
-    first_name).filter(last_name = last_name))
 
-  def die():
-    objects.filter(first_name = first_name).filter(last_name =
-        last_name).delete()
+  first_name = models.CharField(default='first name', max_length = 50) 
+  last_name = models.CharField(default='last name', max_length = 50)
+ 
+  def die(self):
+    self.delete()
 
 
 # I need to implement all of these models, but that is currently for another
